e7aa3bca308645b7360b33cb17f20bf348aff76c
Thiago <tbgt35@hotmail.com>
2023-02-07 16:02:52 -0300

    Atualizando

--- public/index.php
+++ public/index.php
@@ -1,19 +1,61 @@
 <?php
 
-class Pessoa
-{
-    public string $nome;
-    public int $idade;
+//class Pessoa
+//{
+//    public string $nome;
+//    public int $idade;
+//
+//    public function Speak()
+//    {
+//        //Através do $this-> dá para pegar o valor da variável sem ser preciso passar por parametros.
+//        echo $this->nome . " Falou que não tem " . $this->idade;
+//    }
+//}
+//
+//$rodrigo = new Pessoa();
+//$rodrigo->nome = 'Rodrigo Santos';
+//$rodrigo->idade = 18;
+//$rodrigo->Speak();
+//echo $rodrigo->nome;
 
-    public function Speak()
+class Login
+{
+    //Private só pode ser usado dentro da classe
+    private string $email;
+    private int $senha;
+
+    public function getEmail()
     {
-        //Através do $this-> dá para pegar o valor da variável sem ser preciso passar por parametros.
-        echo $this->nome . " Falou que não tem " . $this->idade;
+        return $this->email;
+    }
+
+    public function setEmail($e)
+    {
+        $this->email = $e;
+    }
+
+    public function getSenha()
+    {
+        return $this->senha;
+    }
+
+    public function setSenha($s)
+    {
+        $this->senha = $s;
+    }
+    public function Login()
+    {
+        if ($this->email == "teste@teste.com" and $this->senha == 123456):
+            echo "Logado com sucesso";
+        else:
+            echo "Dados inválidos";
+        endif;
     }
 }
 
-$rodrigo = new Pessoa();
-$rodrigo->nome = 'Rodrigo Santos';
-$rodrigo->idade = 18;
-$rodrigo->Speak();
-echo $rodrigo->nome;
+$logar = new Login();
+$logar->setEmail('teste@teste.com');
+$logar->setSenha(123456);
+$logar->Login();
+$logar->getEmail();
+$logar->getSenha();
